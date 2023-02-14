db078750e62fb3fed895cb4073971e1470a5c4a5
德布罗煜 <1056317718@qq.com>
2023-02-14 11:49:27 +0800

    feat: 【core】新增灰度图二值化函数

--- packages/core/src/main.rs
+++ packages/core/src/main.rs
@@ -9,9 +9,14 @@ fn main() {
         panic!("参数数目有误！");
     }
 
-    let img = imgcodecs::imread(&args[1], imgcodecs::IMREAD_COLOR).expect("读取图片时发生错误");
-    let gray_image = transfer::transfer_to_gray_image(&img);
+    let image = imgcodecs::imread(&args[1], imgcodecs::IMREAD_COLOR).expect("读取图片时发生错误");
+    highgui::imshow("Original_Image", &image).expect("展示图片窗口时发生错误"); // 原图
 
-    highgui::imshow("gray", &gray_image).expect("展示图片窗口时发生错误");
+    let gray_image = transfer::transfer_rgb_image_to_gray_image(&image);
+    highgui::imshow("Gray_Image", &gray_image).expect("展示图片窗口时发生错误"); // 灰度图
+
+    let thresh_image = transfer::transfer_gray_image_to_thresh_binary(&gray_image);
+    highgui::imshow("Thresh_Binary_Image", &thresh_image).expect("展示图片窗口时发生错误"); // 黑白二值图
+
     highgui::wait_key(0).expect("等待函数出错");
 }
--- packages/core/src/transfer.rs
+++ packages/core/src/transfer.rs
@@ -1,10 +1,20 @@
 use opencv::{imgproc, prelude::Mat};
 
 /**
- * 将图片转换成灰度图
+ * 将RGB图片转换成灰度图
  */
-pub fn transfer_to_gray_image(src: &Mat) -> Mat {
+pub fn transfer_rgb_image_to_gray_image(src: &Mat) -> Mat {
     let mut dst = Mat::default();
-    imgproc::cvt_color(src, &mut dst, imgproc::COLOR_RGB2GRAY, 0).expect("灰度转化失败");
+    imgproc::cvt_color(src, &mut dst, imgproc::COLOR_RGB2GRAY, 0).expect("RGB图转灰度图失败");
+    return dst;
+}
+
+/**
+ * 将灰度图转换成黑白二值图
+ */
+pub fn transfer_gray_image_to_thresh_binary(src: &Mat) -> Mat {
+    let mut dst = Mat::default();
+    imgproc::threshold(src, &mut dst, 127.0, 255.0, imgproc::THRESH_BINARY)
+        .expect("灰度图二值化阈值处理失败");
     return dst;
 }
