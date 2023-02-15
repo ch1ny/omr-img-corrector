8194a52dd2cdfbc191bfd37da78fa8bd2f0d661b
德布罗煜 <1056317718@qq.com>
2023-02-15 17:24:50 +0800

    feat: 【app】初始化用户界面代码

--- packages/app/src-tauri/src/main.rs
+++ packages/app/src-tauri/src/main.rs
@@ -3,15 +3,18 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn show_main_window(window: tauri::Window) {
+    // 展示主窗口
+    window.get_window("main").unwrap().show().unwrap();
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![show_main_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,10 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./style.css";
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './style.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
