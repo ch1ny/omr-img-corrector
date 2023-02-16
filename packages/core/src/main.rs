710708b2998b1e61d739292cd4d8c518449cb713
德布罗煜 <1056317718@qq.com>
2023-02-16 16:04:19 +0800

    feat: 【core】新增黑白二值图转换为横向投影图的函数

--- packages/core/src/main.rs
+++ packages/core/src/main.rs
@@ -9,14 +9,29 @@ fn main() {
         panic!("参数数目有误！");
     }
 
-    let image = imgcodecs::imread(&args[1], imgcodecs::IMREAD_COLOR).expect("读取图片时发生错误");
-    highgui::imshow("Original_Image", &image).expect("展示图片窗口时发生错误"); // 原图
+    let mut image = transfer::MyMat::default();
+    image
+        .load_mat(&args[1], imgcodecs::IMREAD_UNCHANGED)
+        .expect("读取图片时发生错误");
+    // image
+    //     .show("Original_Image")
+    //     .expect("展示图片窗口时发生错误"); // 原图
 
-    let gray_image = transfer::transfer_rgb_image_to_gray_image(&image);
-    highgui::imshow("Gray_Image", &gray_image).expect("展示图片窗口时发生错误"); // 灰度图
+    let gray_image = transfer::transfer_rgb_image_to_gray_image(&image).expect("RGB图转灰度图失败");
+    // gray_image
+    //     .show("Gray_Image")
+    //     .expect("展示图片窗口时发生错误"); // 灰度图
 
-    let thresh_image = transfer::transfer_gray_image_to_thresh_binary(&gray_image);
-    highgui::imshow("Thresh_Binary_Image", &thresh_image).expect("展示图片窗口时发生错误"); // 黑白二值图
+    let thresh_image = transfer::transfer_gray_image_to_thresh_binary(&gray_image)
+        .expect("灰度图二值化阈值处理失败");
+    // thresh_image
+    //     .show("Thresh_Binary_Image")
+    //     .expect("展示图片窗口时发生错误"); // 黑白二值图
+
+    let hp = transfer::transfer_thresh_binary_to_horizontal_projection(&thresh_image)
+        .expect("计算纵向投影发生错误");
+    hp.show("Horizontal_Projection")
+        .expect("展示图片窗口时发生错误");
 
     highgui::wait_key(0).expect("等待函数出错");
 }
--- packages/core/src/transfer.rs
+++ packages/core/src/transfer.rs
@@ -1,20 +1,108 @@
-use opencv::{imgproc, prelude::Mat};
+use opencv::{
+    highgui, imgcodecs, imgproc,
+    prelude::{Mat, MatTraitConst, MatTraitConstManual, MatTraitManual},
+};
 
-/**
- * 将RGB图片转换成灰度图
- */
-pub fn transfer_rgb_image_to_gray_image(src: &Mat) -> Mat {
-    let mut dst = Mat::default();
-    imgproc::cvt_color(src, &mut dst, imgproc::COLOR_RGB2GRAY, 0).expect("RGB图转灰度图失败");
-    return dst;
+pub struct MyMat {
+    mat: Mat,
+}
+impl MyMat {
+    /// 生成默认 Mat 对象
+    pub fn default() -> Self {
+        Self {
+            mat: Mat::default(),
+        }
+    }
+
+    /// 根据文件路径加载新的 Mat
+    pub fn load_mat(
+        self: &mut Self,
+        filename: &str,
+        flags: i32,
+    ) -> Result<&mut Self, opencv::Error> {
+        let image = imgcodecs::imread(filename, flags)?;
+        self.mat = image;
+
+        Ok(self)
+    }
+
+    /// getter mat
+    #[allow(dead_code)]
+    pub fn get_mat(self: &Self) -> &Mat {
+        &self.mat
+    }
+
+    pub fn new(mat: Mat) -> Self {
+        Self { mat }
+    }
+
+    /// 利用 opencv::highgui 窗口展示图片
+    pub fn show(self: &Self, win_name: &str) -> Result<(), opencv::Error> {
+        highgui::imshow(win_name, &self.mat)?;
+        Ok(())
+    }
+
+    /// 获取 mat 字节数组
+    #[allow(dead_code)]
+    pub fn get_bytes(self: &Self) -> Result<&[u8], opencv::Error> {
+        self.mat.data_bytes()
+    }
 }
 
-/**
- * 将灰度图转换成黑白二值图
- */
-pub fn transfer_gray_image_to_thresh_binary(src: &Mat) -> Mat {
+/// 将RGB图片转换成灰度图
+pub fn transfer_rgb_image_to_gray_image(src: &MyMat) -> Result<MyMat, opencv::Error> {
     let mut dst = Mat::default();
-    imgproc::threshold(src, &mut dst, 127.0, 255.0, imgproc::THRESH_BINARY)
-        .expect("灰度图二值化阈值处理失败");
-    return dst;
+    imgproc::cvt_color(&src.mat, &mut dst, imgproc::COLOR_RGB2GRAY, 0)?;
+
+    Ok(MyMat::new(dst))
+}
+
+/// 将灰度图转换成黑白二值图
+pub fn transfer_gray_image_to_thresh_binary(src: &MyMat) -> Result<MyMat, opencv::Error> {
+    let mut dst = Mat::default();
+    imgproc::threshold(&src.mat, &mut dst, 127.0, 255.0, imgproc::THRESH_BINARY)?;
+
+    Ok(MyMat::new(dst))
+}
+
+/// 将黑白二值图转换为横向投影图
+pub fn transfer_thresh_binary_to_horizontal_projection(
+    src: &MyMat,
+) -> Result<MyMat, opencv::Error> {
+    // 克隆原图作为目标图片
+    let mut mat = (&src.mat).clone();
+
+    // 遍历每一行
+    for col_index in 0..mat.rows() {
+        // 获取当前行的数据数组
+        let row = mat.at_row_mut::<u8>(col_index)?;
+        // 需要填色的色块次序
+        let mut filled_index = 0;
+        // 是否需要填色
+        // 只有当出现了白色色块时才将此值修改为 true
+        let mut flag = false;
+
+        // 遍历当前行的每一个色块
+        for row_index in 0..src.mat.cols() {
+            // 如果为白色色块
+            // 将 flag 置为 true
+            // 不做其他处理
+            if row[row_index as usize] == 255 {
+                flag = true;
+                continue;
+            }
+
+            // 如果为黑色色块
+            // 且 flag 为 true
+            // 则将本色块置为白色
+            // 并将需要涂抹的色块置为黑色
+            if flag {
+                row[row_index as usize] = 255;
+                row[filled_index] = 0;
+            }
+            filled_index += 1;
+        }
+    }
+
+    Ok(MyMat::new(mat))
 }
