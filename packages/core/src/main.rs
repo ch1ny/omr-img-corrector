f46dc8eead1e106f663f31c51d2ab21e5d65a471
德布罗煜 <1056317718@qq.com>
2023-02-21 14:17:17 +0800

    feat: 【core】计算标准差

--- packages/core/src/main.rs
+++ packages/core/src/main.rs
@@ -1,40 +1,70 @@
-use oics::{ highgui, imgcodecs, transfer };
-use std::env;
+use oics::{
+    core::{self, Scalar},
+    imgcodecs, imgproc, transfer,
+};
+use std::{env, time::Instant};
 
 fn main() {
+    let start_time = Instant::now();
+
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         panic!("参数数目有误！");
     }
 
-    let mut image = transfer::TransformableMat::default();
-    image
-        .load_mat(&args[1], imgcodecs::IMREAD_UNCHANGED)
-        .expect("读取图片时发生错误");
-    image
-        .show("Original_Image")
-        .expect("展示图片窗口时发生错误"); // 原图
+    let time_0 = start_time.elapsed().as_millis();
 
-    let gray_image = transfer::transfer_rgb_image_to_gray_image(&image).expect("RGB图转灰度图失败");
-    gray_image
-        .show("Gray_Image")
-        .expect("展示图片窗口时发生错误"); // 灰度图
+    let mut original_image = transfer::TransformableMat::default();
+    original_image
+        .load_mat(&args[1], imgcodecs::IMREAD_COLOR)
+        .expect("读取图片时发生错误");
+
+    let time_1 = start_time.elapsed().as_millis();
+
+    let gray_image =
+        transfer::transfer_rgb_image_to_gray_image(&original_image).expect("RGB图转灰度图失败");
+
+    let time_2 = start_time.elapsed().as_millis();
 
     let thresh_image = transfer::transfer_gray_image_to_thresh_binary(&gray_image)
         .expect("灰度图二值化阈值处理失败");
-    thresh_image
-        .show("Thresh_Binary_Image")
-        .expect("展示图片窗口时发生错误"); // 黑白二值图
 
-    let hp = transfer::transfer_thresh_binary_to_horizontal_projection(&thresh_image)
-        .expect("计算纵向投影发生错误");
-    hp.show("Horizontal_Projection")
-        .expect("展示图片窗口时发生错误");
+    let time_3 = start_time.elapsed().as_millis();
 
-    let vp = transfer::transfer_thresh_binary_to_vertical_projection(&thresh_image)
-        .expect("计算横向投影发生错误");
-    vp.show("Vertical_Projection")
-        .expect("展示图片窗口时发生错误");
+    let mut results = vec![];
+    let mut times = (vec![], vec![]);
+    for deg in -45..45 {
+        let dr = Instant::now();
 
-    highgui::wait_key(0).expect("等待函数出错");
+        let rotated_image = transfer::rotate_mat(
+            &thresh_image,
+            deg as f64,
+            imgproc::WARP_INVERSE_MAP,
+            core::BORDER_CONSTANT,
+            Scalar::new(255.0, 255.0, 255.0, 0.0), // b g r
+        )
+        .expect("旋转图像时发生错误！");
+
+        let md = dr.elapsed().as_millis();
+
+        let projection_standard_deviations =
+            transfer::get_projection_standard_deviations(&rotated_image)
+                .expect("计算投影标准差时发生错误");
+
+        times.1.push(dr.elapsed().as_millis() - md);
+        times.0.push(md);
+        results.push(projection_standard_deviations);
+    }
+
+    println!("{:?}", results);
+
+    println!("读取参数 => {}", time_0);
+    println!("读取图片 => {}", time_1 - time_0);
+    println!("转灰度图 => {}", time_2 - time_1);
+    println!("二值化处理 => {}", time_3 - time_2);
+
+    println!("旋转图像 => {:?}", times.0);
+    println!("计算标准差 => {:?}", times.1);
+
+    println!("总耗时 => {}", start_time.elapsed().as_millis());
 }
