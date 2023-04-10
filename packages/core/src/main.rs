cb1df2d81e21408c843e0bc0b285d6ac381d8f61
德布罗煜 <1056317718@qq.com>
2023-04-10 19:25:30 +0800

    feat: 【core】改为使用封装好的投影标准差函数

--- packages/core/src/main.rs
+++ packages/core/src/main.rs
@@ -4,177 +4,11 @@ use oics::{
     imgcodecs,
     imgproc,
     // prelude::MatTraitConstManual,
-    transfer::{self, TransformableMatrix},
+    transfer,
     types::{ImageFormat, RotateClipStrategy},
 };
 use rand::Rng;
-use std::{
-    collections::HashMap,
-    path::Path,
-    sync::{Arc, Mutex},
-    thread,
-    time::Instant,
-};
-
-fn get_most_possible_angle(vec: (Vec<f64>, Vec<f64>)) -> usize {
-    // 处理垂直投影数据
-    let vertical_vec = vec.0;
-    let mut vertical_possibles: (f64, Vec<usize>) = (vertical_vec[0], vec![0]);
-    for (index, val) in vertical_vec.iter().enumerate() {
-        if *val > vertical_possibles.0 {
-            vertical_possibles.0 = *val;
-            vertical_possibles.1 = vec![index];
-        } else if *val == vertical_possibles.0 {
-            vertical_possibles.1.push(index);
-        }
-    }
-
-    // 处理水平投影数据
-    let horizontal_vec = vec.1;
-    let mut horizontal_possibles: (f64, Vec<usize>) = (horizontal_vec[0], vec![0]);
-    for (index, val) in horizontal_vec.iter().enumerate() {
-        if *val > horizontal_possibles.0 {
-            horizontal_possibles.0 = *val;
-            horizontal_possibles.1 = vec![index];
-        } else if *val == horizontal_possibles.0 {
-            horizontal_possibles.1.push(index);
-        }
-    }
-
-    // 唯一结果且相等
-    if vertical_possibles.1.len() == 1
-        && horizontal_possibles.1.len() == 1
-        && vertical_possibles.1[0] == horizontal_possibles.1[0]
-    {
-        return vertical_possibles.1[0];
-    }
-
-    let mut candidate_hashmap = HashMap::new();
-    for deg_index in vertical_possibles.1 {
-        candidate_hashmap.entry(deg_index).or_insert((
-            vertical_vec[deg_index as usize],
-            horizontal_vec[deg_index as usize],
-        ));
-    }
-    for deg_index in horizontal_possibles.1 {
-        candidate_hashmap.entry(deg_index).or_insert((
-            vertical_vec[deg_index as usize],
-            horizontal_vec[deg_index as usize],
-        ));
-    }
-
-    let mut sdp = 0.0;
-    let mut most_possible_angle: Option<usize> = None;
-    for (angle, (vsd, hsd)) in candidate_hashmap {
-        let cur_sdp = vsd.powf(2.0) + hsd.powf(2.0);
-        if sdp < cur_sdp {
-            sdp = cur_sdp;
-            most_possible_angle = Some(angle);
-        }
-    }
-
-    match most_possible_angle {
-        Some(value) => value,
-        None => vertical_vec.len() / 2,
-    }
-}
-
-fn find_target_angle(
-    max_angle: u16,
-    step: f64,
-    thresh_image: TransformableMatrix,
-    threads: usize,
-) -> f64 {
-    let max_angle = (max_angle as f64 / step) as u16;
-    let min_angle = -(max_angle as i32);
-    let range = min_angle..(max_angle as i32);
-
-    let standard_deviations = if threads <= 1 {
-        // 单线程
-        let mut standard_deviations = (
-            Vec::with_capacity(range.len()),
-            Vec::with_capacity(range.len()),
-        );
-
-        for deg in range {
-            let rotated_image = transfer::rotate_mat(
-                &thresh_image,
-                deg as f64 * step,
-                1.0,
-                imgproc::WARP_POLAR_LINEAR,
-                core::BORDER_CONSTANT,
-                Scalar::new(255.0, 255.0, 255.0, 0.0), // b g r
-                RotateClipStrategy::DEFAULT,
-            )
-            .expect("旋转图像时发生错误！");
-
-            let projection_standard_deviations =
-                transfer::get_projection_standard_deviations(&rotated_image)
-                    .expect("计算投影标准差时发生错误");
-
-            standard_deviations.0.push(projection_standard_deviations.0);
-            standard_deviations.1.push(projection_standard_deviations.1);
-        }
-
-        standard_deviations
-    } else {
-        // 多线程
-        let mut handles = Vec::with_capacity(threads);
-        let index = Arc::new(Mutex::new(0));
-
-        let arc_standard_deviations =
-            Arc::new(Mutex::new((vec![0.0; range.len()], vec![0.0; range.len()])));
-        let arc_thresh_image = Arc::new(thresh_image);
-        for _ in 0..threads {
-            let ref_standard_deviations = Arc::clone(&arc_standard_deviations);
-            let ref_index = Arc::clone(&index);
-            let ref_thresh_image = Arc::clone(&arc_thresh_image);
-
-            let handle = thread::spawn(move || loop {
-                let angle = {
-                    let mut index_locker = ref_index.lock().unwrap();
-                    let current_index = *index_locker;
-                    if current_index == (max_angle * 2) as i32 {
-                        break;
-                    }
-                    *index_locker = current_index + 1;
-                    current_index + min_angle
-                };
-
-                let rotated_image = transfer::rotate_mat(
-                    &ref_thresh_image,
-                    angle as f64,
-                    1.0,
-                    imgproc::WARP_POLAR_LINEAR,
-                    core::BORDER_CONSTANT,
-                    Scalar::new(255.0, 255.0, 255.0, 0.0), // b g r
-                    RotateClipStrategy::DEFAULT,
-                )
-                .expect("旋转图像时发生错误！");
-
-                let projection_standard_deviations =
-                    transfer::get_projection_standard_deviations(&rotated_image)
-                        .expect("计算投影标准差时发生错误");
-
-                {
-                    let mut rsd = ref_standard_deviations.lock().unwrap();
-                    rsd.0[(angle - min_angle) as usize] = projection_standard_deviations.0;
-                    rsd.1[(angle - min_angle) as usize] = projection_standard_deviations.1;
-                }
-            });
-            handles.push(handle);
-        }
-
-        for handle in handles {
-            handle.join().unwrap();
-        }
-
-        let sd = arc_standard_deviations.lock().unwrap().to_owned();
-        sd
-    };
-
-    return ((get_most_possible_angle(standard_deviations) as f64) - (max_angle as f64)) * step;
-}
+use std::{path::Path, time::Instant};
 
 const DATA_SET_DIR_PATH: &str = "C:/Users/10563/Desktop/dataset";
 fn run_test(p: bool, h: bool, f: bool) {
@@ -214,15 +48,8 @@ fn run_test(p: bool, h: bool, f: bool) {
 
         if p {
             let projection_start = instant.elapsed().as_millis();
-            let thresh_image = {
-                let mut gray_image =
-                    transfer::transfer_rgb_image_to_gray_image(&original_image).unwrap();
-                let gray_image = gray_image.resize_self(0.2).unwrap();
-
-                transfer::transfer_gray_image_to_thresh_binary(&gray_image).unwrap()
-            };
-
-            let projection_angle = find_target_angle(45, 0.2, thresh_image.clone(), 1);
+            let projection_angle =
+                oics::projection::get_angle_with_projections(&original_image, 45, 0.2, 0.2, 1);
 
             let final_image = transfer::rotate_mat(
                 &original_image,
