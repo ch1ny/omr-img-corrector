086e5996b298f1fa4ef073659809a182aeb0c518
德布罗煜 <1056317718@qq.com>
2023-05-15 22:55:32 +0800

    chore(app): 添加注释

--- packages/app/src/hooks/useLocalStorage.ts
+++ packages/app/src/hooks/useLocalStorage.ts
@@ -1,30 +1,36 @@
 import { useCallback, useEffect, useState } from 'react';
 
 interface IUseLocalStorageOptions<T> {
-	serializer?: (value: T) => string;
-	deserializer?: (raw: string) => T;
-	defaultValue?: T | ((previousState?: T) => T);
+	serializer?: (value: T) => string; // 序列化函数
+	deserializer?: (raw: string) => T; // 解序列化函数
+	defaultValue?: T | ((previousState?: T) => T); // 默认值
 }
 
 export default <T>(key: string, options?: IUseLocalStorageOptions<T>) => {
 	const { serializer, deserializer, defaultValue } = options || {};
 
+	// 定义从 localStorage 中取出对应键的值的函数
 	const getStoredValue = useCallback(() => {
 		try {
+			// 从 localStorage 中取出的字符串
 			const raw = localStorage.getItem(key);
 			if (raw) {
+				// 默认使用 JSON.parse 进行序列化
 				return (deserializer || JSON.parse)(raw);
 			}
 		} catch (error) {}
 
 		if (typeof defaultValue === 'function') {
+			// 默认值传入为函数时立即执行，以返回值作为默认值的值
 			return (<(previousState?: T) => T>defaultValue)();
 		}
 		return defaultValue;
 	}, [key, serializer, deserializer, defaultValue]);
 
+	// hook 内部维持状态
 	const [state, setState] = useState<T>(getStoredValue());
 
+	// 定义更新状态函数
 	const updateState = useCallback(
 		(arg: T | ((prevState: T) => T)) => {
 			if (typeof arg === 'function') {
@@ -35,12 +41,14 @@ export default <T>(key: string, options?: IUseLocalStorageOptions<T>) => {
 				});
 			}
 
+			// 更新状态时同时更新 localStorage 的值
 			localStorage.setItem(key, (serializer || JSON.stringify)(arg));
 			return setState(arg);
 		},
 		[key, serializer]
 	);
 
+	// 通过监听 window 对象的 storage 事件实现监听其他窗口改动 localStorage 的值, 以此实现简单的跨窗口通信
 	useEffect(() => {
 		const listener = (ev: StorageEvent) => {
 			const { key: evKey, newValue } = ev;
